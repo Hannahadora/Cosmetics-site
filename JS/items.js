4507d05048895165a1e0c549f898bca33a49df76
Hannahadora <hejimofor141@uniport.edu.ng>
2021-03-31 23:10:18 +0100

    processing

---
+++ JS/items.js
@@ -0,0 +1,42 @@
+
+// POP UP**************************************//
+
+$(document).ready(function() {
+
+    // Initialize the plugin
+    $('#item1').popup();
+
+    // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
+    $.fn.popup.defaults.pagecontainer = '#page';
+
+    });
+
+$(document).ready(function() {
+
+    // Initialize the plugin
+    $('#item2').popup();
+
+    // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
+    $.fn.popup.defaults.pagecontainer = '#page';
+
+    });
+
+$(document).ready(function() {
+
+    // Initialize the plugin
+    $('#item3').popup();
+
+    // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
+    $.fn.popup.defaults.pagecontainer = '#page';
+
+    });
+
+$(document).ready(function() {
+
+    // Initialize the plugin
+    $('#item4').popup();
+
+    // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
+    $.fn.popup.defaults.pagecontainer = '#page';
+
+    });
